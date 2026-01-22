290201e8453f3127391fc63971d2ae6690595245
ElektrifiedDev <160866233+ElektrifiedDev@users.noreply.github.com>
2026-01-22 23:09:10 +0000

    Initialize PyCasino project structure
    Added main application logic in src/main.py, including settings and save file management, and basic UI initialization using PyQt5 and pywebview. Created initial UI files: index.html, coinflip.html, app.js, and styles.css to set up the foundation for the PyCasino application.

---
+++ src/main.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import json
+import hashlib
+import random
+import webview as pywebview
+import PyQt5
+import uuid
+import datetime
+
+from datetime import datetime
+from PyQt5 import QtWidgets, QtCore, QtGui, QtWebEngineWidgets
+
+USER_APPDATA_FOLDER = os.getenv('APPDATA')
+APPDATA_PYCASINO_PATH = os.path.join(USER_APPDATA_FOLDER, 'PyCasino')
+PYCASINO_STORAGE_PATH = os.path.join(APPDATA_PYCASINO_PATH, 'data')
+SETTINGS_FILE_PATH = os.path.join(PYCASINO_STORAGE_PATH, 'settings.json')
+SAVES_FOLDER_PATH = os.path.join(PYCASINO_STORAGE_PATH, 'saves')
+
+UIPATH = "./ui/index.html"
+
+if not os.path.exists(APPDATA_PYCASINO_PATH):
+    os.makedirs(APPDATA_PYCASINO_PATH)
+if not os.path.exists(PYCASINO_STORAGE_PATH):
+    os.makedirs(PYCASINO_STORAGE_PATH)
+if not os.path.exists(SAVES_FOLDER_PATH):
+    os.makedirs(SAVES_FOLDER_PATH)
+
+def load_settings():
+    if not os.path.exists(SETTINGS_FILE_PATH):
+        settings = {
+            "volume": 50,
+            "language": "en",
+        }
+        with open(SETTINGS_FILE_PATH, 'w') as f:
+            json.dump(settings, f)
+
+    with open(SETTINGS_FILE_PATH, 'r') as f:
+        settings = json.load(f)
+
+    return settings
+
+def fetch_saves():
+    if not os.path.exists(SAVES_FOLDER_PATH):
+        os.makedirs(SAVES_FOLDER_PATH)
+
+    if not os.listdir(SAVES_FOLDER_PATH):
+        create_save_file()
+
+    save_files = [f for f in os.listdir(SAVES_FOLDER_PATH) if f.endswith('.json')]
+    return save_files
+
+def create_save_file():
+    HARDWARE_ID = str(uuid.getnode())
+    DATETIME_NOW = datetime.now()
+    
+    default_save = {
+        "balance": 100,
+        "metadata" : {
+            "HardwareID": HARDWARE_ID,
+            "DTN": str(DATETIME_NOW)
+        }
+    }
+
+    save_file_id = hashlib.md5((HARDWARE_ID + str(DATETIME_NOW)).encode()).hexdigest()[::20]
+
+    save_file_name = f'PyCasino_{save_file_id}.json'
+    SAVE_FILE_PATH = os.path.join(SAVES_FOLDER_PATH, save_file_name)
+    with open(SAVE_FILE_PATH, 'w') as f:
+        json.dump(default_save, f)
+
+def initialize_game():
+    window = pywebview.create_window("PyCasino", UIPATH, width=800, height=600)
+    window.expose(fetch_saves, load_settings)
+    pywebview.start(gui='qt')
+
+if __name__ == "__main__":
+    initialize_game()
